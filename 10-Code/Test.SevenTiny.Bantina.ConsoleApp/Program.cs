b12dc6c36349bc1233a40757b8c718c7eed16f65
qixiao <wd8622088@foxmail.com>
2018-02-09 00:22:07 +0800

    initialize solution

--- 10-Code/Test.SevenTiny.Bantina.ConsoleApp/Program.cs
+++ 10-Code/Test.SevenTiny.Bantina.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Test.SevenTiny.Bantina.Model;
+using static Newtonsoft.Json.JsonConvert;
 
 namespace Test.SevenTiny.Bantina.ConsoleApp
 {
@@ -6,7 +8,16 @@ namespace Test.SevenTiny.Bantina.ConsoleApp
     {
         static void Main(string[] args)
         {
+            School school = new School();
+            Console.WriteLine(SerializeObject(school));
+
+            //bankinate要改成restful风格接口和实现
+
             Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("any key to exit ...");
+            Console.ReadKey();
         }
     }
 }
