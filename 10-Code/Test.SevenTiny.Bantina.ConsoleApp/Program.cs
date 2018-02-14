69918f528dae9b45b7e74349d375c32f7ca7d7e6
sevenTiny <sevenTiny@foxmail.com>
2018-02-14 23:45:32 +0800

    Revert "Dev"

--- 10-Code/Test.SevenTiny.Bantina.ConsoleApp/Program.cs
+++ 10-Code/Test.SevenTiny.Bantina.ConsoleApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using Test.SevenTiny.Bantina.Model;
 using static Newtonsoft.Json.JsonConvert;
-using SevenTiny.Configuration;
-using Microsoft.Extensions.Configuration;
 
 namespace Test.SevenTiny.Bantina.ConsoleApp
 {
@@ -10,17 +8,13 @@ namespace Test.SevenTiny.Bantina.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //School school = new School();
-            //Console.WriteLine(SerializeObject(school));
+            School school = new School();
+            Console.WriteLine(SerializeObject(school));
 
             //bankinate要改成restful风格接口和实现
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(TestConfig.Get("li"));
-            }
-            
 
             Console.WriteLine("Hello World!");
+
 
             Console.WriteLine("any key to exit ...");
             Console.ReadKey();
