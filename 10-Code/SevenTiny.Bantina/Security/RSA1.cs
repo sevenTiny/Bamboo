5d2e833a1c50635d4970c2e5ebec876e2c925500
sevenTiny <sevenTiny@foxmail.com>
2018-04-08 19:19:52 +0800

    rsa add

---
+++ 10-Code/SevenTiny.Bantina/Security/RSA1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SevenTiny.Bantina.Security
+{
+    /// <summary>
+    /// SHA1WithRSA,no impose restrictions on length
+    /// </summary>
+    public class RSA1
+    {
+        
+    }
+}
+}
---
+++ 10-Code/SevenTiny.Bantina/Security/RSA2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SevenTiny.Bantina.Security
+{
+    /// <summary>
+    /// SHA256WithRSA,impose restrictions on length no more less 2048
+    /// </summary>
+    public class RSA2
+    {
+    }
+}
