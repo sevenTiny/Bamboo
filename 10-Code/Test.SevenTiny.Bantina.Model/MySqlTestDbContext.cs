0bfe36612486160479808182bc982d2f8a04c459
seventiny <seventiny@foxmail.com>
2018-04-23 20:59:59 +0800

    modify localcache default false;

--- 10-Code/Test.SevenTiny.Bantina.Model/MySqlTestDbContext.cs
+++ 10-Code/Test.SevenTiny.Bantina.Model/MySqlTestDbContext.cs
@@ -9,6 +9,7 @@ namespace Test.SevenTiny.Bantina.Model
     {
         public MySqlTestDbContext() : base("server=101.201.66.247;Port=39901;database=test;uid=root;pwd=CYj(9yyz*8;Allow User Variables=true;")
         {
+            LocalCache = true;
         }
     }
 }
