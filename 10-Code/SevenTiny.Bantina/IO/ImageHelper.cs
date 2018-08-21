f733b5ba3133b94d3a580412791a3fe906d7d2eb
sevenTiny <sevenTiny@foxmail.com>
2018-08-21 23:50:15 +0800

    add helpers

---
+++ 10-Code/SevenTiny.Bantina/IO/ImageHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SevenTiny.Bantina.IO
+{
+    public class ImageHelper
+    {
+        /// <summary>
+        ///image file convert to byte array
+        /// </summary>
+        /// <param name="path">image path</param>
+        /// <returns>byte array result</returns>
+        public static Byte[] ImageToByteArray(string path)
+        {
+            using (var fs = File.OpenRead(path))
+            {
+                int filelength = 0;
+                filelength = (int)fs.Length;
+                Byte[] imagebyte = new Byte[filelength];
+                fs.Read(imagebyte, 0, filelength);
+                fs.Close();
+                return imagebyte;
+            }
+        }
+        /// <summary>
+        /// image file convert to base64 string
+        /// </summary>
+        /// <param name="path">image path</param>
+        /// <returns>base64 string result</returns>
+        public static string ImageToBase64(string path)
+        {
+            var byteArr = ImageToByteArray(path);
+            return Convert.ToBase64String(byteArr);
+        }
+        /// <summary>
+        /// base64 to image
+        /// </summary>
+        /// <param name="base64">base64 string</param>
+        /// <param name="path">store path</param>
+        public static void Base64ToImage(string base64, string path)
+        {
+            string filepath = Path.GetDirectoryName(path);
+            if (!Directory.Exists(filepath))
+            {
+                if (filepath != null) Directory.CreateDirectory(filepath);
+            }
+            var match = Regex.Match(base64, "data:image/png;base64,([\\w\\W]*)$");
+            if (match.Success)
+            {
+                base64 = match.Groups[1].Value;
+            }
+            var photoBytes = Convert.FromBase64String(base64);
+            File.WriteAllBytes(path, photoBytes);
+        }
+        /// <summary>
+        /// check image is jpg
+        /// </summary>
+        /// <param name="imageBase64">image base64 string</param>
+        /// <returns>true is jpg  false not jpg</returns>
+        public static bool IsJPG(string imageBase64)
+        {
+            var img = Convert.FromBase64String(imageBase64);
+            var jpgStr = $"{img[0].ToString()}{img[1].ToString()}";
+            return jpgStr.Equals($"{(int)ImageFormat.JPG}");
+        }
+        /// <summary>
+        /// check image is png
+        /// </summary>
+        /// <param name="imageBase64">image base64 string</param>
+        /// <returns>true is png  false not png</returns>
+        public static bool IsPNG(string imageBase64)
+        {
+            var img = Convert.FromBase64String(imageBase64);
+            var jpgStr = $"{img[0].ToString()}{img[1].ToString()}";
+            return jpgStr.Equals($"{(int)ImageFormat.PNG}");
+        }
+        /// <summary>
+        /// check image is gif
+        /// </summary>
+        /// <param name="imageBase64">image base64 string</param>
+        /// <returns>true is gif  false not gif</returns>
+        public static bool IsGIF(string imageBase64)
+        {
+            var img = Convert.FromBase64String(imageBase64);
+            var jpgStr = $"{img[0].ToString()}{img[1].ToString()}";
+            return jpgStr.Equals($"{(int)ImageFormat.GIF}");
+        }
+
+        private enum ImageFormat
+        {
+            JPG = 255216,
+            GIF = 7173,
+            PNG = 13780,
+        }
+    }
+}
---
+++ 10-Code/SevenTiny.Bantina/IO/ZipHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Compression;
+
+namespace SevenTiny.Bantina.IO
+{
+    public class ZipHelper
+    {
+        /// <summary>
+        /// zip file
+        /// </summary>
+        /// <param name="sourcePath">file path to zip</param>
+        /// <param name="zipPath">zip file path</param>
+        public static void Zip(string sourcePath, string zipPath)
+        {
+            ZipFile.CreateFromDirectory(sourcePath, zipPath);
+        }
+        /// <summary>
+        /// exact file
+        /// </summary>
+        /// <param name="zipPath">zip file path</param>
+        /// <param name="exactPath">exact file path</param>
+        public static void UnZip(string zipPath, string exactPath)
+        {
+            ZipFile.ExtractToDirectory(zipPath, exactPath);
+        }
+    }
+}
