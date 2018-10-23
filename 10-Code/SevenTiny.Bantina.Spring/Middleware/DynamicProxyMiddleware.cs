202c813bd5447b6bbce8e7f72590d32c28805893
sevenTiny <sevenTiny@foxmail.com>
2018-10-23 16:58:45 +0800

    ajust middleware

--- 10-Code/SevenTiny.Bantina.Spring/Middleware/DynamicProxyMiddleware.cs
+++ 10-Code/SevenTiny.Bantina.Spring/Middleware/DynamicProxyMiddleware.cs
@@ -5,6 +5,7 @@ namespace SevenTiny.Bantina.Spring.Middleware
 {
     public static class DynamicProxyMiddleware
     {
+        private static readonly InterceptorBaseAttribute interceptor = new InterceptorBaseAttribute();
         public static void UseDynamicProxy(this IApplicationBuilder app)
         {
             app.UseDynamicProxy(context => true);
@@ -16,7 +17,7 @@ namespace SevenTiny.Bantina.Spring.Middleware
             {
                 if (beforeCheck(context))
                 {
-                    context.Result = new InterceptorBaseAttribute().Invoke(context.InstanceObject, context.Method, context.Parameters);
+                    context.Result = interceptor.Invoke(context.InstanceObject, context.Method, context.Parameters);
                 }
                 return next();
             });
