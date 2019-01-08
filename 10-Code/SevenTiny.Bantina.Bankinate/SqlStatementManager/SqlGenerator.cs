82b16ff283d154397892c2a32889c7560488b1ad
seventiny <seventiny@foxmail.com>
2019-01-08 23:53:28 +0800

    修复mysql 的update语句的参数化查询拼接错误的bug:zap: 修复update语句中如果是key标签标记过的列则无法修改的bug:zap:

--- 10-Code/SevenTiny.Bantina.Bankinate/SqlStatementManager/SqlGenerator.cs
+++ 10-Code/SevenTiny.Bantina.Bankinate/SqlStatementManager/SqlGenerator.cs
@@ -92,8 +92,8 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
 
         public static string Update<TEntity>(DbContext dbContext, Expression<Func<TEntity, bool>> filter, TEntity entity, out IDictionary<string, object> paramsDic) where TEntity : class
         {
-            dbContext.TableName = TableAttribute.GetName(typeof(TEntity));
             paramsDic = new Dictionary<string, object>();
+            dbContext.TableName = TableAttribute.GetName(typeof(TEntity));
 
             StringBuilder builder_front = new StringBuilder();
             builder_front.Append("UPDATE ");
@@ -103,7 +103,10 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 builder_front.Append(dbContext.TableName);
                 builder_front.Append(" ");
             }
-            builder_front.Append(filter.Parameters[0].Name);
+
+            //查询语句中表的别名，例如“t”
+            string alias = filter.Parameters[0].Name;
+            builder_front.Append(alias);
             builder_front.Append(" SET ");
 
             PropertyInfo[] propertyInfos = GetPropertiesDicByType(typeof(TEntity));
@@ -114,16 +117,12 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 if (propertyInfo.GetCustomAttribute(typeof(AutoIncreaseAttribute), true) is AutoIncreaseAttribute autoIncreaseAttr)
                 {
                 }
-                //AutoIncrease : if property is auto increase attribute skip this column.
-                else if (propertyInfo.GetCustomAttribute(typeof(KeyAttribute), true) is KeyAttribute keyAttr)
-                {
-                }
                 //Column :
                 else if (propertyInfo.GetCustomAttribute(typeof(ColumnAttribute), true) is ColumnAttribute columnAttr)
                 {
                     builder_front.Append(columnAttr.GetName(propertyInfo.Name));
                     builder_front.Append("=");
-                    builder_front.Append("@");
+                    builder_front.Append($"@{alias}");
                     //multitype database support
                     switch (dbContext.DataBaseType)
                     {
@@ -140,10 +139,8 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                     }
                     builder_front.Append(columnName);
                     builder_front.Append(",");
-                    if (!paramsDic.ContainsKey(columnName))
-                    {
-                        paramsDic.Add(columnName, propertyInfo.GetValue(entity));
-                    }
+
+                    paramsDic.AddOrUpdate($"@{alias}{columnName}", propertyInfo.GetValue(entity));
                 }
                 //in the end,remove the redundant symbol of ','
                 if (propertyInfos.Last() == propertyInfo)
@@ -158,7 +155,7 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 builder_front.Append(" FROM ");
                 builder_front.Append(dbContext.TableName);
                 builder_front.Append(" ");
-                builder_front.Append(filter.Parameters[0].Name);
+                builder_front.Append(alias);
             }
 
             //Generate SqlStatement
@@ -167,29 +164,35 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
 
         public static string Update<TEntity>(DbContext dbContext, TEntity entity, out IDictionary<string, object> paramsDic, out Expression<Func<TEntity, bool>> filter) where TEntity : class
         {
+            paramsDic = new Dictionary<string, object>();
             dbContext.TableName = TableAttribute.GetName(typeof(TEntity));
             PropertyInfo[] propertyInfos = GetPropertiesDicByType(typeof(TEntity));
 
-            //查找主键以及主键对应的值
+            //查找主键以及主键对应的值，如果用该方法更新数据，主键是必须存在的
             //get property which is key
             var keyProperty = propertyInfos.Where(t => t.GetCustomAttribute(typeof(KeyAttribute), true) is KeyAttribute)?.FirstOrDefault();
             if (keyProperty == null)
                 throw new TableKeyNotFoundException($"table '{dbContext.TableName}' not found key column");
 
-            string colunmName = keyProperty.Name;
-            var value = keyProperty.GetValue(entity);
+            //主键的key
+            string keyName = keyProperty.Name;
+            //主键的value 
+            var keyValue = keyProperty.GetValue(entity);
 
             if (keyProperty.GetCustomAttribute(typeof(ColumnAttribute), true) is ColumnAttribute columnAttr1)
-                colunmName = columnAttr1.GetName(keyProperty.Name);
+                keyName = columnAttr1.GetName(keyProperty.Name);
 
-            //Generate Expression of update via key
+            //Generate Expression of update via key : t=>t.'Key'== value
             ParameterExpression param = Expression.Parameter(typeof(TEntity), "t");
             MemberExpression left = Expression.Property(param, keyProperty);
-            ConstantExpression right = Expression.Constant(value);
+            ConstantExpression right = Expression.Constant(keyValue);
             BinaryExpression where = Expression.Equal(left, right);
             filter = Expression.Lambda<Func<TEntity, bool>>(where, param);
 
-            paramsDic = new Dictionary<string, object>();
+            //将主键的查询参数加到字典中
+            paramsDic.AddOrUpdate($"@t{keyName}", keyValue);
+
+            //开始构造赋值的sql语句
             StringBuilder builder_front = new StringBuilder();
             builder_front.Append("UPDATE ");
             //Mysql和sqlserver的分别处理
@@ -198,7 +201,11 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 builder_front.Append(dbContext.TableName);
                 builder_front.Append(" ");
             }
-            builder_front.Append(filter.Parameters[0].Name);
+
+            //查询语句中表的别名，例如“t”
+            string alias = filter.Parameters[0].Name;
+
+            builder_front.Append(alias);
             builder_front.Append(" SET ");
 
             string columnName = string.Empty;
@@ -208,16 +215,12 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 if (propertyInfo.GetCustomAttribute(typeof(AutoIncreaseAttribute), true) is AutoIncreaseAttribute autoIncreaseAttr)
                 {
                 }
-                //AutoIncrease : if property is auto increase attribute skip this column.
-                else if (propertyInfo.GetCustomAttribute(typeof(KeyAttribute), true) is KeyAttribute keyAttr)
-                {
-                }
                 //Column :
                 else if (propertyInfo.GetCustomAttribute(typeof(ColumnAttribute), true) is ColumnAttribute columnAttr)
                 {
                     builder_front.Append(columnAttr.GetName(propertyInfo.Name));
                     builder_front.Append("=");
-                    builder_front.Append("@");
+                    builder_front.Append($"@{alias}");
                     //multitype database support
                     switch (dbContext.DataBaseType)
                     {
@@ -234,10 +237,8 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                     }
                     builder_front.Append(columnName);
                     builder_front.Append(",");
-                    if (!paramsDic.ContainsKey(columnName))
-                    {
-                        paramsDic.Add(columnName, propertyInfo.GetValue(entity));
-                    }
+
+                    paramsDic.AddOrUpdate($"@{alias}{columnName}", propertyInfo.GetValue(entity));
                 }
                 //in the end,remove the redundant symbol of ','
                 if (propertyInfos.Last() == propertyInfo)
@@ -252,7 +253,7 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
                 builder_front.Append(" FROM ");
                 builder_front.Append(dbContext.TableName);
                 builder_front.Append(" ");
-                builder_front.Append(filter.Parameters[0].Name);
+                builder_front.Append(alias);
             }
 
             //Generate SqlStatement
@@ -288,7 +289,7 @@ namespace SevenTiny.Bantina.Bankinate.SqlStatementManager
             {
                 case DataBaseType.SqlServer:
                 case DataBaseType.MySql:
-                    dbContext.SqlStatement = $"DELETE {filter.Parameters[0].Name} From {dbContext.TableName} {filter.Parameters[0].Name} {LambdaToSql.ConvertWhere(filter,out parameters)}";
+                    dbContext.SqlStatement = $"DELETE {filter.Parameters[0].Name} From {dbContext.TableName} {filter.Parameters[0].Name} {LambdaToSql.ConvertWhere(filter, out parameters)}";
                     break;
                 case DataBaseType.Oracle:
                     dbContext.SqlStatement = string.Empty;
