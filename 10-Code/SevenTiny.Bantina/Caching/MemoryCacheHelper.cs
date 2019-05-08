19b0496fd01df07c68355bd3ac81df777e376d16
sevenTiny <sevenTiny@foxmail.com>
2019-05-08 14:46:40 +0800

    重构MemoryCacheHelper

--- 10-Code/SevenTiny.Bantina/Caching/MemoryCacheHelper.cs
+++ 10-Code/SevenTiny.Bantina/Caching/MemoryCacheHelper.cs
@@ -24,16 +24,10 @@ namespace SevenTiny.Bantina.Caching
         public static TValue Put<TKey, TValue>(TKey key, TValue value) => _cache.Set(key, value);
         public static TValue Put<TKey, TValue>(TKey key, TValue value, TimeSpan absoluteExpirationRelativeToNow) => _cache.Set(key, value, absoluteExpirationRelativeToNow);
         public static TValue Put<TKey, TValue>(TKey key, TValue value, DateTime absoluteExpiration) => _cache.Set(key, value, absoluteExpiration - DateTime.Now);
-        public static TValue Get<TKey, TValue>(TKey key)
-        {
-            if (Exist(key))
-            {
-                return _cache.Get<TValue>(key);
-            }
-            return default(TValue);
-        }
+        public static TValue Get<TKey, TValue>(TKey key) => Exist(key, out TValue value) ? value : default(TValue);
 
         public static bool Exist<TKey>(TKey key) => _cache.TryGetValue(key, out object value);
+        public static bool Exist<TKey, TValue>(TKey key, out TValue value) => _cache.TryGetValue(key, out value);
         public static void Delete<TKey>(TKey key) => _cache.Remove(key);
     }
 }
