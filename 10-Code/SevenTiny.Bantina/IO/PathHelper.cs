632a1728754b7135e14b5fc1d0a020759bd9060d
sevenTiny <sevenTiny@foxmail.com>
2019-05-08 15:14:57 +0800

    TimeHelepr重构:zap:

--- 10-Code/SevenTiny.Bantina/IO/PathHelper.cs
+++ 10-Code/SevenTiny.Bantina/IO/PathHelper.cs
@@ -14,7 +14,7 @@
  *********************************************************/
 using System;
 
-namespace SevenTiny.Bantina
+namespace SevenTiny.Bantina.IO
 {
     public abstract class PathHelper
     {
