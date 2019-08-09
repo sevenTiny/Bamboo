e392024f88f4ebc69f54696e14f5bad78db6f62f
sevenTiny <sevenTiny@foxmail.com>
2019-08-09 11:37:15 +0800

    添加扩展方法，移除旧的不利于理解的分页方法

--- 10-Code/SevenTiny.Bantina/Extensions/CollectionsExtensions.cs
+++ 10-Code/SevenTiny.Bantina/Extensions/CollectionsExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SevenTiny.Bantina.Extensions
 {
@@ -19,6 +20,26 @@ namespace SevenTiny.Bantina.Extensions
         {
             foreach (var item in source)
                 action(item);
+        }
+
+        /// <summary>
+        /// paging source collection to collection for collection
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sources">source collection</param>
+        /// <param name="pageSize">page size</param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<TResult>> Paging<TResult>(this IEnumerable<TResult> sources, int pageSize)
+        {
+            IList<IEnumerable<TResult>> result = new List<IEnumerable<TResult>>();
+            int pageIndex = 0;
+            int count = sources?.Count() ?? 0;
+            while (count > (pageSize * pageIndex))
+            {
+                result.Add(sources.Skip(pageIndex * pageSize).Take(pageSize));
+                pageIndex++;
+            }
+            return result;
         }
     }
 }
