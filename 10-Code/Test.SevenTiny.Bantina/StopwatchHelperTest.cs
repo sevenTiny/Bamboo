079b10d10ebdd9a598abf76563929df01e96353d
sevenTiny <sevenTiny@foxmail.com>
2019-08-09 11:51:19 +0800

    add stopwatch helper unit test

---
+++ 10-Code/Test.SevenTiny.Bantina/StopwatchHelperTest.cs
@@ -0,0 +1,40 @@
+﻿using SevenTiny.Bantina;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace Test.SevenTiny.Bantina
+{
+    public class StopwatchHelperTest
+    {
+        [Theory]
+        [InlineData(100)]
+        [InlineData(200)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        [InlineData(2000)]
+        public void Caculate(int millisecondsTimeout)
+        {
+            var timespan = StopwatchHelper.Caculate(() =>
+            {
+                Thread.Sleep(millisecondsTimeout);
+            });
+            Assert.True(timespan > TimeSpan.FromMilliseconds(millisecondsTimeout));
+        }
+
+        [Theory]
+        [InlineData(3, 100)]
+        [InlineData(3, 200)]
+        [InlineData(3, 300)]
+        [InlineData(3, 400)]
+        [InlineData(3, 500)]
+        public void CaculateTimes(int times, int millisecondsTimeout)
+        {
+            var timespan = StopwatchHelper.Caculate(times, () =>
+             {
+                 Thread.Sleep(millisecondsTimeout);
+             });
+            Assert.True(timespan > TimeSpan.FromMilliseconds(times * millisecondsTimeout));
+        }
+    }
+}
