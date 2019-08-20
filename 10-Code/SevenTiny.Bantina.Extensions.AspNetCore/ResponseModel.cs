c930b0927212a282402cd90a3339a4573d6f1b25
sevenTiny <sevenTiny@foxmail.com>
2019-08-20 10:42:54 +0800

    add ResponseModel:smile:

--- 10-Code/SevenTiny.Bantina.Extensions.AspNetCore/ResponseModel.cs
+++ 10-Code/SevenTiny.Bantina.Extensions.AspNetCore/ResponseModel.cs
@@ -3,26 +3,32 @@ using Newtonsoft.Json;
 
 namespace SevenTiny.Bantina.Extensions.AspNetCore
 {
+    /// <summary>
+    /// 通用的前端交互实体
+    /// </summary>
     [Serializable]
     public class ResponseModel
     {
         [JsonProperty("success")]
         public bool IsSuccess { get; set; }
+        [JsonProperty("code")]
+        public int Code { get; set; } = 200;
         [JsonProperty("msg")]
         public string Message { get; set; }
-        [JsonProperty("more_message")]
+        [JsonProperty("more_msg")]
         public string MoreMessage { get; set; }
         [JsonProperty("tip_type")]
-        public TipType TipType { get; }
-        [JsonProperty("")]
+        public TipType TipType { get; set; }
+        [JsonProperty("data", NullValueHandling = NullValueHandling.Ignore)]
         public object Data { get; set; }
 
         public static ResponseModel Success(object data, string message = null)
             => new ResponseModel
             {
                 IsSuccess = true,
-                Data = data,
-                Message = message
+                Message = message,
+                TipType = TipType.Success,
+                Data = data
             };
 
         public static ResponseModel Error(string message, object data = null)
@@ -30,26 +36,8 @@ namespace SevenTiny.Bantina.Extensions.AspNetCore
             {
                 IsSuccess = false,
                 Message = message,
+                TipType = TipType.Error,
                 Data = data
-            };
-    }
-
-    public static class ResponseModelExtension
-    {
-        public static ResponseModel ToResponseModel(this Result result)
-            =>
-            new ResponseModel
-            {
-                IsSuccess = result.IsSuccess,
-                Message = result.Message,
-            };
-        public static ResponseModel ToResponseModel<T>(this Result<T> result)
-            =>
-            new ResponseModel
-            {
-                IsSuccess = result.IsSuccess,
-                Message = result.Message,
-                Data = result.Data
             };
     }
 }
---
+++ 10-Code/SevenTiny.Bantina.Extensions.AspNetCore/ResponseModelExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace SevenTiny.Bantina.Extensions.AspNetCore
+{
+    public static class ResponseModelExtensions
+    {
+        public static ResponseModel ToResponseModel(this Result result)
+            =>
+            new ResponseModel
+            {
+                IsSuccess = result.IsSuccess,
+                Message = result.Message,
+                MoreMessage = result.MoreMessage,
+                TipType = result.TipType
+            };
+
+        public static ResponseModel ToResponseModel<T>(this Result<T> result)
+            =>
+            new ResponseModel
+            {
+                IsSuccess = result.IsSuccess,
+                Message = result.Message,
+                MoreMessage = result.MoreMessage,
+                TipType = result.TipType,
+                Data = result.Data
+            };
+    }
+}
