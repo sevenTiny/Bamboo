8d560c4bf4ed5f80c168eed84c270e396df7a5c1
sevenTiny <sevenTiny@foxmail.com>
2019-09-12 17:30:05 +0800

    add Ensure:cup:

---
+++ 10-Code/SevenTiny.Bantina/Validation/Ensure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SevenTiny.Bantina.Validation
+{
+    public static class Ensure
+    {
+        [System.Diagnostics.DebuggerStepThrough]
+        public static void ArgumentNotNullOrEmpty(object arg, string argName, string message = null)
+        {
+            bool valid = true;
+
+            if (arg is string a && string.IsNullOrEmpty(a))
+                valid = false;
+
+            else if (arg is IEnumerable<int> enumerable_int && (enumerable_int == null || !enumerable_int.Any()))
+                valid = false;
+
+            else if (arg is IEnumerable<float> enumerable_float && (enumerable_float == null || !enumerable_float.Any()))
+                valid = false;
+
+            else if (arg is IEnumerable<double> enumerable_double && (enumerable_double == null || !enumerable_double.Any()))
+                valid = false;
+
+            else if (arg is IEnumerable<object> enumerable && (enumerable == null || !enumerable.Any()))
+                valid = false;
+
+            else if (arg == null)
+                valid = false;
+
+            if (!valid)
+                throw new ArgumentNullException(nameof(argName), message ?? "Parameter cannot be null or empty");
+        }
+    }
+}
---
+++ 10-Code/Test.SevenTiny.Bantina/EnsureTest.cs
@@ -0,0 +1,55 @@
+﻿using SevenTiny.Bantina.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test.SevenTiny.Bantina
+{
+    public class EnsureTest
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ArgumentNotNullOrEmpty(object data)
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(data, nameof(data)); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty2()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new object[] { }, null); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty3()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new string[] { }, null); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty4()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new double[] { }, null); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty5()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new float[] { }, null); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty6()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new dynamic[] { }, null); });
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty7()
+        {
+            Assert.Throws<ArgumentNullException>(() => { Ensure.ArgumentNotNullOrEmpty(new List<object> { }, null); });
+        }
+    }
+}
