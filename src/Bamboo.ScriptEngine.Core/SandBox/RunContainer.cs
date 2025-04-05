33f31cce8a54d2a4d6f362b66ee2da2ec5fa1dd5
seventiny <seventiny@foxmail.com>
2025-04-05 15:27:41 +0800

    remove dependency of login

--- src/Bamboo.ScriptEngine.Core/SandBox/RunContainer.cs
+++ src/Bamboo.ScriptEngine.Core/SandBox/RunContainer.cs
@@ -1,29 +1,18 @@
 ﻿using Fasterflect;
-using Microsoft.Extensions.Logging;
-using Bamboo.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Bamboo.ScriptEngine.SandBox
+namespace Bamboo.ScriptEngine.Core.SandBox
 {
     [Serializable]
     internal class RunContainer : MarshalByRefObject
     {
-        private static readonly ILogger _logger = new BambooLogger<RunContainer>();
-
         public object ExecuteUntrustedCode(Type type, string methodName, params object[] parameters)
         {
             var obj = Activator.CreateInstance(type);
-            try
-            {
-                return obj.TryCallMethodWithValues(methodName, parameters);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"call method [{methodName}] error.");
-            }
-            return null;
+
+            return obj.TryCallMethodWithValues(methodName, parameters);
         }
 
         public object ExecuteUntrustedCode(Type type, string methodName, int millisecondsTimeout, params object[] parameters)
@@ -37,8 +26,8 @@ namespace Bamboo.ScriptEngine.SandBox
             if (!t.Wait(millisecondsTimeout, token))
             {
                 tokenSource.Cancel();
-                _logger.LogError(string.Format("[Tag:{0},Method:{1},Timeout:{2}, execution timed out", type.Assembly.FullName, methodName, millisecondsTimeout));
-                return null;
+
+                throw new TimeoutException(string.Format("[Assembly:{0},Method:{1},Timeout:{2}, execution timed out", type.Assembly.FullName, methodName, millisecondsTimeout));
             }
             return result;
         }
